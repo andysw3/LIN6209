3110a1c0c91b4eb97be1e8b971b70e2f670b1d64
andy <afsanchezw@gmail.com>
2024-10-20 16:25:44 +0100

    question1&2assignment

--- andy.py
+++ andy.py
@@ -1,1 +1,14 @@
-print("hey")
+
+def all_ops_part1(int1,int2):
+    return (int1+int2, int1-int2, int1 * int2, int1 / int2, int1 // int2, int1 % int2)
+
+#all_ops_part1(5,3)
+
+def all_ops_part2(int1,int2):
+    return (int1<int2, int1<=int2, int1==int2, int1!=int2, int1>=int2, int1>int2)
+
+#all_ops_part2(5,3)
+
+assert all_ops_part1(5,3) == (8, 2, 15, 5/3, 1, 2)
+
+assert all_ops_part2(5,3) == (False,False,False,True,True,True)
